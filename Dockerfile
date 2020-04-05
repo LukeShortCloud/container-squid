d9a978f99fb4a980bd2522850307e0c4c038d701
Luke Short <ekultails@gmail.com>
2020-04-05 14:30:46 -0400

    Use CentOS 8 and simplify the container.
    Squid will now allow connections from any host and
    will also correctly use the cache directory.

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,11 @@
-FROM ubuntu:bionic-20190612
-LABEL maintainer="sameer@damagehead.com"
+FROM centos:8.1.1911
 
-ENV SQUID_VERSION=3.5.27 \
-    SQUID_CACHE_DIR=/var/spool/squid \
-    SQUID_LOG_DIR=/var/log/squid \
-    SQUID_USER=proxy
-
-RUN apt-get update \
- && DEBIAN_FRONTEND=noninteractive apt-get install -y squid=${SQUID_VERSION}* \
- && rm -rf /var/lib/apt/lists/*
+LABEL maintainer="ekultails@gmail.com"
 
 COPY entrypoint.sh /sbin/entrypoint.sh
-RUN chmod 755 /sbin/entrypoint.sh
+
+RUN yum install -y squid && yum clean all && chmod 755 /sbin/entrypoint.sh
 
 EXPOSE 3128/tcp
+
 ENTRYPOINT ["/sbin/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,37 +1,27 @@
 #!/bin/bash
 set -e
 
-create_log_dir() {
-  mkdir -p ${SQUID_LOG_DIR}
-  chmod -R 755 ${SQUID_LOG_DIR}
-  chown -R ${SQUID_USER}:${SQUID_USER} ${SQUID_LOG_DIR}
-}
-
-create_cache_dir() {
-  mkdir -p ${SQUID_CACHE_DIR}
-  chown -R ${SQUID_USER}:${SQUID_USER} ${SQUID_CACHE_DIR}
-}
-
-create_log_dir
-create_cache_dir
-
 # allow arguments to be passed to squid
 if [[ ${1:0:1} = '-' ]]; then
   EXTRA_ARGS="$@"
   set --
-elif [[ ${1} == squid || ${1} == $(which squid) ]]; then
+elif [[ ${1} == squid || ${1} == "/usr/sbin/squid" ]]; then
   EXTRA_ARGS="${@:2}"
   set --
 fi
 
 # default behaviour is to launch squid
 if [[ -z ${1} ]]; then
-  if [[ ! -d ${SQUID_CACHE_DIR}/00 ]]; then
+
+  if [[ ! -d /var/spool/squid/00 ]]; then
     echo "Initializing cache..."
-    $(which squid) -N -f /etc/squid/squid.conf -z
+    /usr/sbin/squid -N -f /etc/squid/squid.conf -z
   fi
+
+  sed -i s'/^http_access.*/http_access allow all/'g /etc/squid/squid.conf
+  echo "cache_dir ufs /var/spool/squid 100 16 256" >> /etc/squid/squid.conf
   echo "Starting squid..."
-  exec $(which squid) -f /etc/squid/squid.conf -NYCd 1 ${EXTRA_ARGS}
+  exec /usr/sbin/squid -f /etc/squid/squid.conf -NYCd 1 ${EXTRA_ARGS}
 else
   exec "$@"
 fi
